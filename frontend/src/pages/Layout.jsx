2bc1fbd85e268a8a3bfbf93d91e0b7e9a89d4e5f
Matías Gutiérrez <ma.gutierrez.or@gmail.com>
2022-10-03 16:53:07 -0300

    added the container padding to the add UI

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,47 +1,49 @@
+import Container from 'react-bootstrap/Container'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 import Form from 'react-bootstrap/Form'
-import { useEffect, useState } from 'react'
 
 export default function Layout() {
   const navigate = useNavigate()
   const handleRadioButtonCheck = (event) => {
-        navigate(`/${event.target.id}`)
+    navigate(`/${event.target.id}`)
   }
   return (
     <div>
-      <Helmet>
-        <title>matolink</title>
-      </Helmet>
-      <div className='container align-items-centered'>
-        <br />
-        <h1>Sistema de Notas</h1>
-        <br />
-        <Form>
+      <Container>
+        <Helmet>
+          <title>matolink</title>
+        </Helmet>
+        <div className='align-items-centered'>
           <br />
-          {['radio'].map((type) => (
-            <div key={`inline-${type}`} className='mb-3'>
-              <Form.Check
-                onClick={handleRadioButtonCheck}
-                inline
-                label='por RUT'
-                name='group1'
-                type={type}
-                id={'byrut'}
-              />
-              <Form.Check
-                onClick={handleRadioButtonCheck}
-                inline
-                label='por Curso Entero'
-                name='group1'
-                type={type}
-                id={'bysubject'}
-              />
-            </div>
-          ))}
-        </Form>
-        <Outlet />
-      </div>
+          <h1>Sistema de Notas</h1>
+          <br />
+          <Form>
+            <br />
+            {['radio'].map((type) => (
+              <div key={`inline-${type}`} className='mb-3'>
+                <Form.Check
+                  onClick={handleRadioButtonCheck}
+                  inline
+                  label='por RUT'
+                  name='group1'
+                  type={type}
+                  id={'byrut'}
+                />
+                <Form.Check
+                  onClick={handleRadioButtonCheck}
+                  inline
+                  label='por Curso Entero'
+                  name='group1'
+                  type={type}
+                  id={'bysubject'}
+                />
+              </div>
+            ))}
+          </Form>
+          <Outlet />
+        </div>
+      </Container>
     </div>
   )
 }
--- frontend/src/pages/PostToDb.jsx
+++ frontend/src/pages/PostToDb.jsx
@@ -1,3 +1,4 @@
+import Container from 'react-bootstrap/Container'
 import { useEffect, useState } from 'react'
 import { Form } from 'react-bootstrap'
 import { Outlet, useNavigate } from 'react-router-dom'
@@ -17,64 +18,69 @@ export default function PostToDb() {
   }
   return (
     <div>
-      <Helmet>
-        <title>matolink</title>
-      </Helmet>
-      <Form>
-        {['radio'].map((type) => (
-          <div key={`inline-${type}`} className='mb-3'>
-            <Form.Check
-              onClick={handleRadioButtonCheck}
-              inline
-              label='Agregar Profesor'
-              name='group1'
-              type={type}
-              id={'addteacher'}
-            />
-            <Form.Check
-              onClick={handleRadioButtonCheck}
-              inline
-              label='Agregar Alumno'
-              name='group1'
-              type={type}
-              id={'addstudent'}
-            />
-            <Form.Check
-              onClick={handleRadioButtonCheck}
-              inline
-              label='Añadir Alumno a curso'
-              name='group1'
-              type={type}
-              id={'addstusub'}
-            />
-            <Form.Check
-              onClick={handleRadioButtonCheck}
-              inline
-              label='Añadir Curso'
-              name='group1'
-              type={type}
-              id={'addsubject'}
-            />
-            <Form.Check
-              onClick={handleRadioButtonCheck}
-              inline
-              label='Añadir Nota'
-              name='group1'
-              type={type}
-              id={'addgrade'}
-            />
-            <Form.Check
-              onClick={handleRadioButtonCheck}
-              inline
-              label='Añadir Examen'
-              name='group1'
-              type={type}
-              id={'addexam'}
-            />
-          </div>
-        ))}
-      </Form>
-      <Outlet />
+      <Container>
+          <br />
+          <h1>Sistema de Notas</h1>
+          <br />
+        <Helmet>
+          <title>matolink</title>
+        </Helmet>
+        <Form>
+          {['radio'].map((type) => (
+            <div key={`inline-${type}`} className='mb-3'>
+              <Form.Check
+                onClick={handleRadioButtonCheck}
+                inline
+                label='Agregar Profesor'
+                name='group1'
+                type={type}
+                id={'addteacher'}
+              />
+              <Form.Check
+                onClick={handleRadioButtonCheck}
+                inline
+                label='Agregar Alumno'
+                name='group1'
+                type={type}
+                id={'addstudent'}
+              />
+              <Form.Check
+                onClick={handleRadioButtonCheck}
+                inline
+                label='Añadir Alumno a curso'
+                name='group1'
+                type={type}
+                id={'addstusub'}
+              />
+              <Form.Check
+                onClick={handleRadioButtonCheck}
+                inline
+                label='Añadir Curso'
+                name='group1'
+                type={type}
+                id={'addsubject'}
+              />
+              <Form.Check
+                onClick={handleRadioButtonCheck}
+                inline
+                label='Añadir Nota'
+                name='group1'
+                type={type}
+                id={'addgrade'}
+              />
+              <Form.Check
+                onClick={handleRadioButtonCheck}
+                inline
+                label='Añadir Examen'
+                name='group1'
+                type={type}
+                id={'addexam'}
+              />
+            </div>
+          ))}
+        </Form>
+        <Outlet />
+      </Container>
     </div>
   )
 }
